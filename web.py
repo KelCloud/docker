9bdccd3b70eea447eb1f5b7f39f3ae73e5f0c7b5
ipangoi <ipangoirifan12@gmail.com>
2023-06-08 18:15:26 +0700

    add port and fix billing

--- web.py
+++ web.py
@@ -16,11 +16,14 @@ class Container(db.Model):
     id = db.Column(db.String(255), primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     image = db.Column(db.String(255), nullable=False)
+    port = db.Column(db.String(255), nullable=False)
+    
 
-    def __init__(self, id, name, image):
+    def __init__(self, id, name, image, port):
         self.id = id
         self.name = name
         self.image = image
+        self.port = port
 
     @property
     def status(self):
@@ -29,24 +32,39 @@ class Container(db.Model):
         except:
             return 'not found'
         return container.status
-    
+
     def calculate_billing(self):
-        start_time = Billing.query.filter_by(container_id=self.id).order_by(Billing.start_time.desc()).first()
-        if start_time:
-            diff = datetime.now() - start_time.start_time
-            seconds = diff.total_seconds()
-            billing_amount = seconds * 1
-            return billing_amount
+        # Check if the container is running
+        container_status = self.status
+        if container_status == 'running':
+            latest_billing = Billing.query.filter_by(container_id=self.id).order_by(Billing.start_time.desc()).first()
+            if latest_billing:
+                diff = datetime.now() - latest_billing.start_time
+                seconds = diff.total_seconds()
+                self.billing_amount = int(seconds) * 1
+                return self.billing_amount
+        elif container_status == 'exited':
+            total_billing = 0
+            billings = Billing.query.filter_by(container_id=self.id).all()
+            for billing in billings:
+                diff = billing.stop_time - billing.start_time
+                seconds = diff.total_seconds()
+                billing_amount = int(seconds) * 1
+                total_billing += billing_amount
+                self.total_billing=total_billing
+                self.billing_amount=billing_amount
+            return self.total_billing,self.billing_amount
         return 0
+
 
 
 class Billing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     container_id = db.Column(db.String(255), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False)
-    stop_time = db.Column(db.DateTime, nullable=False)
+    stop_time = db.Column(db.DateTime, nullable=True)
 
-    def __init__(self, container_id, start_time, stop_time):
+    def __init__(self, container_id, start_time, stop_time=None):
         self.container_id = container_id
         self.start_time = start_time
         self.stop_time = stop_time
@@ -63,13 +81,18 @@ def create_container():
     if request.method == 'POST':
         name = request.form['name']
         image = request.form['image']
-        docker_container = client.containers.run(image, detach=True)
-        container = Container(docker_container.id, name, image)
+        port = request.form['port']  # Retrieve the port value from the form
+
+        # Start the container with the specified port
+        docker_container = client.containers.run(image, detach=True, ports={'80/tcp': port})
+
+        # Create a Container object and save it in the database
+        container = Container(docker_container.id, name, image, port)
         db.session.add(container)
         db.session.commit()
         flash(f'Container Done!! (id = {docker_container.id})')
-    return render_template("create_container.html")
 
+    return render_template("create_container.html")
 
 @app.route('/start_container/<container_id>')
 def start_container(container_id):
@@ -77,6 +100,10 @@ def start_container(container_id):
     docker_container = client.containers.get(container_id)
     docker_container.start()
     flash('Successfully started container')
+    billing = Billing(container_id=container_id, start_time=datetime.now(), stop_time=None)
+    db.session.add(billing)
+    db.session.commit()
+
     return redirect(url_for('index'))
 
 
@@ -87,10 +114,10 @@ def stop_container(container_id):
     docker_container.stop()
     flash('Successfully stopped container')
 
-    # Tambahkan entri billing
-    billing = Billing(container_id=container_id, start_time=datetime.now(), stop_time=datetime.now())
-    db.session.add(billing)
-    db.session.commit()
+    billing = Billing.query.filter_by(container_id=container_id, stop_time=None).first()
+    if billing:
+        billing.stop_time = datetime.now()
+        db.session.commit()
 
     return redirect(url_for('index'))
 
